b565e4c07781527161a86bf76f8bb05455978758
Abhijit Mukherjee <mabhijit.contact@gmail.com>
2020-08-11 17:34:43 +0530

    API first Commit

---
+++ routes/api/auth.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const { model } = require("mongoose");
+const router = express.Router();
+
+/* 
+    @route      GET api/auth
+    @desc       Test route
+    @access     Public
+*/
+router.get("/", (req, res) => {
+    res.send("Auth Route");
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const app = express();
-
 const PORT = process.env.PORT || 5000;
+// call db connection from config
+const connectDB = require("./config/db");
 
+//connect to Altas DB
+connectDB();
+
+//init Middleware
+app.use(express.json({ extended: false }));
+
+//Define Routes and Main routes to access root
+// domain.com/
+// domain.com/api/{routename}
 app.get('/', (req, res) => {
     res.send("API is running");
 });
+
+app.use("/api/users", require('./routes/api/users'));
+app.use("/api/auth", require('./routes/api/auth'));
+app.use("/api/profile", require('./routes/api/profile'));
+app.use("/api/posts", require('./routes/api/posts'));
 
 app.listen(PORT, () => {
     console.log(`server is running on ${PORT}`);
